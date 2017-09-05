b07e82b654e3859d2abd0cc347793374d2aea23d
Lee Sung Jae <me@lsj.pink>
2017-09-05 19:51:16 +0900

    Hello, world!

---
+++ Chapter_02/src/Problem_03.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Problem_03 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		int a, b, result;
+		char k;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("첫번째 계산 할 값을 입력합니다. => ");
+		a = s.nextInt();
+		
+		System.out.print("+ - * / % => ");
+		k = (char) System.in.read();
+
+		System.out.print("두번째 계산 할 값을 입력합니다. => ");
+		b = s.nextInt();
+		
+		switch(k) {
+		case '+':
+			result = a + b;
+			System.out.println(a + " + " + b + " = " + result);
+		
+		case '-':
+			result = a - b;
+			System.out.println(a + " - " + b + " = " + result);
+			
+		case '*':
+			result = a * b;
+			System.out.println(a + " * " + b + " = " + result);
+			
+		case '/':
+			if(b != 0) {
+				result = a / b;
+				System.out.println(a + " / " + b + " = " + result);
+			} else System.out.println("0으로 나눌 수 없습니다!");
+			
+		case '%':
+			if(b != 0) {
+				result = a % b;
+				System.out.println(a + " % " + b + " = " + result);
+			} else System.out.println("0으로 나눌 수 없습니다!");
+		}
+		
+	}
+
+}

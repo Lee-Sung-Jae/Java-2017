f9e3fddeb355839a53df91d4e1d074ce52558847
Lee Sung Jae <me@lsj.pink>
2017-09-12 19:40:59 +0900

    Today is it

---
+++ Chapter_04/src/Ex04_03.java
@@ -0,0 +1,15 @@
+
+public class Ex04_03 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		
+		a++;
+		System.out.printf("%d", ++a);
+		System.out.printf("%d", a);
+
+		System.out.printf("%d", a--);
+		System.out.printf("%d", a);
+	}
+
+}
---
+++ Chapter_04/src/Ex04_05.java
@@ -0,0 +1,16 @@
+
+public class Ex04_05 {
+
+	public static void main(String[] args) {
+		int a = 100, b = 200;
+
+		System.out.printf("%d == %d는 %s다.\n", a, b, 100 == b);
+		System.out.printf("%d != %d는 %s다.\n", a, b, 100 != b);
+		System.out.printf("%d >  %d는 %s다.\n", a, b, 100 > b);
+		System.out.printf("%d <  %d는 %s다.\n", a, b, 100 < b);
+		System.out.printf("%d >= %d는 %s다.\n", a, b, 100 >= b);
+		System.out.printf("%d <= %d는 %s다.\n", a, b, 100 <= b);
+		// System.out.printf("%d = %d는 %s다.\n", a, b, a = b);
+	}
+
+}

eec51e767f031f9c29295ade2b5011045e5ed824
Lee Sung Jae <me@lsj.pink>
2018-02-28 20:57:22 +0900

    Add JavaDoc in NumberProcess class.

--- Personal/src/pink/lsj/study2017/java/testscoreprocess/NumberProcess.java
+++ Personal/src/pink/lsj/study2017/java/testscoreprocess/NumberProcess.java
@@ -1,11 +1,23 @@
 package pink.lsj.study2017.java.testscoreprocess;
 
 class NumberProcess {
+	/**
+	 * Parse Integer type number.
+	 *
+	 * @param numberStr String to parse to number.
+	 * @return Parsed Integer type number.
+	 */
 	static int[] parseInt(String numberStr) {
 		if(numberStr.matches("([^0-9])+") || numberStr.isEmpty()) return new int[] {0, -1};
 		else return new int[] {1, (Integer.parseInt(numberStr))};
 	}
 
+	/**
+	 * Parse Double type number.
+	 *
+	 * @param numberStr String to parse to number.
+	 * @return Parsed Double type number.
+	 */
 	static double[] parseDouble(String numberStr) {
 		if(numberStr.matches("([^0-9.])+") || numberStr.isEmpty()) return new double[] {0, -1};
 		else return new double[] {1, (Double.parseDouble(numberStr))};
